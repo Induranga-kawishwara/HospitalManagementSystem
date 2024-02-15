115bbb0bf8bd163a6affedae2f6f69914f824412
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-02-15 10:27:01 +0530

    update

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,14 +5,10 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import StaffMembers from "./scenes/Staff/StaffMembers";
 import Patients from "./scenes/patients/Patients";
-import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
-import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
@@ -34,7 +30,6 @@ function App() {
               <Route path="/team" element={<StaffMembers />} />
               <Route path="/patients" element={<Patients />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
--- FrontEnd/src/scenes/dashboard/index.jsx
+++ FrontEnd/src/scenes/dashboard/index.jsx
@@ -8,8 +8,6 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
-import GeographyChart from "../../components/GeographyChart";
-import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 
@@ -255,9 +253,7 @@ const Dashboard = () => {
           >
             Sales Quantity
           </Typography>
-          <Box height="250px" mt="-20px">
-            <BarChart isDashboard={true} />
-          </Box>
+          <Box height="250px" mt="-20px"></Box>
         </Box>
         <Box
           gridColumn="span 4"
@@ -273,7 +269,7 @@ const Dashboard = () => {
             Geography Based Traffic
           </Typography>
           <Box height="200px">
-            <GeographyChart isDashboard={true} />
+            {/* <GeographyChart isDashboard={true} /> */}
           </Box>
         </Box>
       </Box>
