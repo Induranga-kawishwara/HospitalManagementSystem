2a0217f7c4a6b38c939a05a173d4632849fb1a04
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-02-20 18:56:35 +0530

    update the code

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,16 +1,22 @@
 import express from "express";
-//const express = require('express');
 import bodyParser from "body-parser";
 import userRouters from "./routes/users.js";
+import mongoose from "mongoose";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
 
 const app = express();
 const PORT = 5000;
 app.use(bodyParser.json());
 app.use("/users", userRouters);
 
+mongoose.connect(process.env.MONGODB).then(() => console.log("connected"));
+
 app.get("/", (req, res) => {
   res.send("Hello from Homepage");
 });
+
 app.listen(PORT, () =>
   console.log(`Server running on port : http://localhost:${PORT}`)
 );
