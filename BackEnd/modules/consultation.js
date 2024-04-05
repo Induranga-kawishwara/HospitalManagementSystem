17586a2e5c405f5151e7afc64062377329a5b975
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-04-06 02:20:39 +0530

    update

--- BackEnd/modules/consultation.js
+++ BackEnd/modules/consultation.js
@@ -3,26 +3,26 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const ConsultationSchema = new Schema({
-  patientId: {
-    type: String,
-    required: true,
-  },
   doctorId: {
     type: String,
     required: true,
   },
-  consultationDate: {
-    type: Date,
-    required: true,
-  },
-  consultationTime: {
-    type: String,
-    required: true,
-  },
-  contactNum: {
-    type: String,
-    required: true,
-  },
+  consultations: [
+    {
+      patientId: {
+        type: String,
+        required: true,
+      },
+      consultationDate: {
+        type: Date,
+        required: true,
+      },
+      contactNum: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 export default model("Consultations", ConsultationSchema);
--- BackEnd/modules/staffMember.js
+++ BackEnd/modules/staffMember.js
@@ -2,39 +2,50 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const StaffSchema = new Schema({
-  staffID: {
-    type: String,
-    required: true,
+const StaffSchema = new Schema(
+  {
+    staffID: {
+      type: String,
+      required: true,
+      unique: true, // Ensure staff ID is unique across all entries
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    staffType: {
+      type: String,
+      required: true,
+      enum: ["Doctor", "Nurse", "Cleaner", "Administrative", "Other"], // Define allowable roles
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    roleDetails: {
+      department: { type: String },
+      shift: { type: String },
+      specialization: { type: String },
+    },
   },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  phoneNum: {
-    type: String,
-    required: true,
-  },
-  addressOne: {
-    type: String,
-    required: true,
-  },
-  addressTwo: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-export default model("StaffMembers", StaffSchema);
+export default model("StaffMember", StaffSchema);
