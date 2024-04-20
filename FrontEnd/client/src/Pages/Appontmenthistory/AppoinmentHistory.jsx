0d38d47ea2f636ee808ec6330cae58b6bf3fa4be
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-04-20 10:07:52 +0530

    updated

--- FrontEnd/client/src/Pages/Appontmenthistory/AppoinmentHistory.jsx
+++ FrontEnd/client/src/Pages/Appontmenthistory/AppoinmentHistory.jsx
@@ -1,34 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { setConsultations } from "../../redux/actions";
+import { useSelector } from "react-redux";
 import axios from "axios";
 import Footer from "../../components/Footer/TheFooter.jsx";
 
 import AppoinmentCard from "../../components/AppoinmentCard/AppoinmentCard";
 
 function AppoinmentHistory() {
+  console.log("ddd");
   const { patientId } = useParams();
-  const dispatch = useDispatch();
-  const consultationsList = useSelector((state) => state.consultations);
+  const [consultationsList, setConsultations] = useState([]);
   const doctorList = useSelector((state) => state.doctors);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!consultationsList.length) {
-        try {
-          const result = await axios.get(
-            `http://localhost:5000/consultations/${patientId}`
-          );
-          dispatch(setConsultations(result.data));
-        } catch (error) {
-          console.error("Failed to fetch data:", error);
-        }
+      try {
+        const result = await axios.get(
+          `http://localhost:5000/consultations/${patientId}`
+        );
+        setConsultations(result.data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
       }
     };
 
     fetchData();
-  }, [consultationsList.length, dispatch, patientId]);
+  }, [patientId]); // Only run useEffect when patientId changes
 
   return (
     <div
@@ -40,43 +37,40 @@ function AppoinmentHistory() {
       }}
     >
       <ul style={{ listStyle: "none" }}>
-        <form>
-          {consultationsList.map((it) =>
-            it.consultations.map((pat, index) => {
-              // Find the scheduled doctor using doctorId
-              const scheduledDoctor = doctorList.find(
-                (doctor) => doctor._id === it.doctorId
-              );
+        {consultationsList.map((it, index) =>
+          it.consultations.map((pat) => {
+            const scheduledDoctor = doctorList.find(
+              (doctor) => doctor._id === it.doctorId
+            );
+            console.log(pat);
 
+            if (pat.status === "scheduled") {
               return (
-                <div>
-                  <li key={index} style={{ margin: 30 }}>
-                    <AppoinmentCard
-                      people={{
-                        avatar: "",
-                        qr: "/qr.png",
-                        displayName: `${scheduledDoctor.firstName} ${scheduledDoctor.lastName}`,
-                        tagline: "",
-                        specialize: ` Specialize - ${pat.specialization}`,
-                        date: `Date - ${new Date(
-                          pat.consultationDateAndTime
-                        ).toLocaleDateString()}`,
-                        time: `Time - ${new Date(
-                          pat.consultationDateAndTime
-                        ).toLocaleTimeString([], {
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })}`,
-                        location: `Hospital Location - ${pat.branchName}`,
-                      }}
-                    />
-                  </li>
+                <div key={index} style={{ margin: 30 }}>
+                  <AppoinmentCard
+                    people={{
+                      avatar: "",
+                      qr: "/qr.png",
+                      displayName: `${scheduledDoctor.firstName} ${scheduledDoctor.lastName}`,
+                      tagline: "",
+                      specialize: ` Specialize - ${pat.specialization}`,
+                      date: `Date - ${new Date(
+                        pat.consultationDateAndTime
+                      ).toLocaleDateString()}`,
+                      time: `Time - ${new Date(
+                        pat.consultationDateAndTime
+                      ).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}`,
+                      location: `Hospital Location - ${pat.branchName}`,
+                    }}
+                  />
                 </div>
               );
-            })
-          )}
-        </form>
-        ;
+            }
+          })
+        )}
       </ul>
     </div>
   );
