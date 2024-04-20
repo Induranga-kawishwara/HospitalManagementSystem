e7b2c2cc2fc340567deb106cb4c1ab768a932971
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-04-20 19:43:33 +0530

    updated

--- FrontEnd/client/src/Pages/Appontmenthistory/AppoinmentHistory.jsx
+++ FrontEnd/client/src/Pages/Appontmenthistory/AppoinmentHistory.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { setDoctors } from "../../redux/actions";
+import style from "./NotFoundPage.module.css";
+import { Link } from "react-router-dom"; // Add this line to import Link
+
 import AppoinmentCard from "../../components/AppoinmentCard/AppoinmentCard";
 
 function AppoinmentHistory() {
@@ -62,44 +65,61 @@ function AppoinmentHistory() {
         marginTop: "120px",
       }}
     >
-      <ul style={{ listStyle: "none" }}>
-        {consultationsList.map((it, index) =>
-          it.consultations.map((pat) => {
-            const scheduledDoctor = doctorList.find(
-              (doctor) => doctor._id === it.doctorId
-            );
-
-            if (pat.status === "scheduled") {
-              return (
-                <div key={index} style={{ margin: 30 }}>
-                  <AppoinmentCard
-                    people={{
-                      avatar: "",
-                      qr: "/qr.png",
-                      displayName: `${scheduledDoctor.firstName} ${scheduledDoctor.lastName}`,
-                      tagline: "",
-                      specialize: ` Specialize - ${pat.specialization}`,
-                      date: `Date - ${new Date(
-                        pat.consultationDateAndTime
-                      ).toLocaleDateString()}`,
-                      time: `Time - ${new Date(
-                        pat.consultationDateAndTime
-                      ).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}`,
-                      location: `Hospital Location - ${pat.branchName}`,
-                    }}
-                    id={pat._id}
-                    onDelete={handleDelete}
-                  />
-                </div>
+      {consultationsList.length === 0 ? (
+        <div className={style.notFoundContainer}>
+          <h1 className={style.notFoundText}>No Booked Consultations</h1>
+          {/* <img
+              src={notFoundImage}
+              alt="Not Found"
+              className={style.notFoundImage}
+            /> */}
+          <p className={style.notFoundText}>
+            Sorry, it seems you don't have any booked consultations.
+          </p>
+          <Link to="/" className={style.goBackButton}>
+            Go Back to Home
+          </Link>
+        </div>
+      ) : (
+        <ul style={{ listStyle: "none" }}>
+          {consultationsList.map((it, index) =>
+            it.consultations.map((pat) => {
+              const scheduledDoctor = doctorList.find(
+                (doctor) => doctor._id === it.doctorId
               );
-            }
-            return null;
-          })
-        )}
-      </ul>
+
+              if (pat.status === "scheduled") {
+                return (
+                  <div key={index} style={{ margin: 30 }}>
+                    <AppoinmentCard
+                      people={{
+                        avatar: "",
+                        qr: "/qr.png",
+                        displayName: `${scheduledDoctor.firstName} ${scheduledDoctor.lastName}`,
+                        tagline: "",
+                        specialize: ` Specialize - ${pat.specialization}`,
+                        date: `Date - ${new Date(
+                          pat.consultationDateAndTime
+                        ).toLocaleDateString()}`,
+                        time: `Time - ${new Date(
+                          pat.consultationDateAndTime
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}`,
+                        location: `Hospital Location - ${pat.branchName}`,
+                      }}
+                      id={pat._id}
+                      onDelete={handleDelete}
+                    />
+                  </div>
+                );
+              }
+              return null;
+            })
+          )}
+        </ul>
+      )}
     </div>
   );
 }
--- FrontEnd/client/src/Pages/DoneAppoinment/DoneAppoinment.jsx
+++ FrontEnd/client/src/Pages/DoneAppoinment/DoneAppoinment.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppoinmentCard from "../../components/AppoinmentCard/AppoinmentCard";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setDoctors } from "../../redux/actions";
 import axios from "axios";
+import style from "./NotFoundPage.module.css"; // Assuming you have a CSS file for styling
+import { Link } from "react-router-dom"; // Add this line to import Link
 
 function DoneAppoinment() {
   const { patientId } = useParams();
-
   const [consultationsList, setConsultations] = useState([]);
-
   const doctorList = useSelector((state) => state.doctors);
   const dispatch = useDispatch();
 
@@ -56,56 +56,69 @@ function DoneAppoinment() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "",
-        justifyContent: "center",
-        marginTop: "120px",
-      }}
-    >
-      <ul style={{ listStyle: "none" }}>
-        {consultationsList.map((it, index) =>
-          it.consultations.map((pat) => {
-            const scheduledDoctor = doctorList.find(
-              (doctor) => doctor._id === it.doctorId
-            );
-            if (pat.status === "done") {
-              return (
-                <div key={index} style={{ margin: 30 }}>
-                  <AppoinmentCard
-                    people={{
-                      avatar: "",
-                      qr: "/qr.png",
-                      displayName:
-                        scheduledDoctor.firstName +
-                        " " +
-                        scheduledDoctor.lastName,
-                      tagline: "",
-                      specialize: pat.specialization,
-                      date: new Date(
-                        pat.consultationDateAndTime
-                      ).toLocaleDateString(),
-                      time: new Date(
-                        pat.consultationDateAndTime
-                      ).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      }),
-                      location: pat.branchName,
-                    }}
-                    from="history"
-                    id={pat._id}
-                    onDelete={handleDelete}
-                  />
-                </div>
-              );
-            }
-            return null;
-          })
-        )}
-      </ul>
+    <div className={style.container}>
+      {consultationsList.length === 0 ? (
+        <div className={style.notFoundContainer}>
+          <h1 className={style.notFoundText}>No Previous Consultations</h1>
+          {/* <img
+                src={notFoundImage}
+                alt="Not Found"
+                className={style.notFoundImage}
+              /> */}
+          <p className={style.notFoundText}>
+            Sorry, it seems you don't have any previous consultations.
+          </p>
+          <Link to="/" className={style.goBackButton}>
+            Go Back to Home
+          </Link>
+        </div>
+      ) : (
+        <div className={style.appointments}>
+          <ul style={{ listStyle: "none" }}>
+            {consultationsList.map((it, index) =>
+              it.consultations.map((pat) => {
+                const scheduledDoctor = doctorList.find(
+                  (doctor) => doctor._id === it.doctorId
+                );
+                if (pat.status === "done") {
+                  return (
+                    <div key={index} style={{ margin: 30 }}>
+                      <AppoinmentCard
+                        people={{
+                          avatar: "",
+                          qr: "/qr.png",
+                          displayName:
+                            scheduledDoctor.firstName +
+                            " " +
+                            scheduledDoctor.lastName,
+                          tagline: "",
+                          specialize: pat.specialization,
+                          date: new Date(
+                            pat.consultationDateAndTime
+                          ).toLocaleDateString(),
+                          time: new Date(
+                            pat.consultationDateAndTime
+                          ).toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          }),
+                          location: pat.branchName,
+                        }}
+                        from="history"
+                        id={pat._id}
+                        onDelete={handleDelete}
+                      />
+                    </div>
+                  );
+                }
+                return null;
+              })
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
+
 export default DoneAppoinment;
