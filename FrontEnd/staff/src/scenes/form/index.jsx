a70b7b130973a526ccab59414d540a7d83abd9f9
Sandin Kodagoda <155352466+sandinkodagoda@users.noreply.github.com>
2024-04-23 00:09:36 +0530

    fuck wela

--- FrontEnd/staff/src/scenes/form/index.jsx
+++ FrontEnd/staff/src/scenes/form/index.jsx
@@ -5,8 +5,9 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 import { Input } from "@mui/material"; // Inside your component
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Checkbox, FormControlLabel } from "@mui/material";
 
-const Form = () => {
+const Farm = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
   const handleFormSubmit = (values) => {
@@ -100,6 +101,27 @@ const Form = () => {
                 </Select>
               </FormControl>
 
+              <FormControl
+                fullWidth
+                variant="filled"
+                sx={{ gridColumn: "span 4" }}
+              >
+                <InputLabel id="gender-label">Position</InputLabel>
+                <Select
+                  labelId="position"
+                  id="position"
+                  label="Position"
+                  name="position" // Name of the input (optional, used for form submission)
+                >
+                  <MenuItem value="male">Doctor </MenuItem>
+                  <MenuItem value="female">Admin</MenuItem>
+                  <MenuItem value="other">Cleaner</MenuItem>
+                  <MenuItem value="male">Nurse </MenuItem>
+                  <MenuItem value="female">Admin</MenuItem>
+                  <MenuItem value="other">Other</MenuItem>
+                </Select>
+              </FormControl>
+
               <TextField
                 fullWidth
                 variant="filled"
@@ -188,7 +210,183 @@ const Form = () => {
                   <MenuItem value="other">Other</MenuItem>
                 </Select>
               </FormControl>
+
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{ gridColumn: "span 4" }}
+              />
+
+              {/* <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Select Days"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.selectedDays}
+                name="selectedDays"
+                error={!!touched.selectedDays && !!errors.selectedDays}
+                helperText={touched.selectedDays && errors.selectedDays}
+                sx={{ gridColumn: "span 4" }}
+              /> */}
+
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Select Days"
+                onBlur={handleBlur}
+                value={values.selectedDays}
+                name="selectedDays"
+                error={!!touched.selectedDays && !!errors.selectedDays}
+                helperText={touched.selectedDays && errors.selectedDays}
+                sx={{ gridColumn: "span 4" }}
+              />
+
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Mo"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Mo"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Tu"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Tu"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="We"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="We"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Th"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Th"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Fr"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Fr"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Sa"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Sa"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Su"
+                name="selectedDays"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value="Su"
+              />
+              <FormControl
+                fullWidth
+                variant="filled"
+                sx={{ gridColumn: "span 2" }}
+              >
+                <InputLabel id="gender-label">Starting Hour</InputLabel>
+                <Select
+                  labelId="hospital-label"
+                  id="hospital"
+                  label="Hospital Branch"
+                  name="gender" // Name of the input (optional, used for form submission)
+                >
+                  {Array.from(Array(24).keys()).map((hour) => (
+                    <MenuItem key={hour} value={hour + 1}>
+                      {hour + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl
+                fullWidth
+                variant="filled"
+                sx={{ gridColumn: "span 2" }}
+              >
+                <InputLabel id="gender-label">Ending Hour</InputLabel>
+                <Select
+                  labelId="hospital-label"
+                  id="hospital"
+                  label="Hospital Branch"
+                  name="gender" // Name of the input (optional, used for form submission)
+                >
+                  {Array.from(Array(24).keys()).map((hour) => (
+                    <MenuItem key={hour} value={hour + 1}>
+                      {hour + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl
+                fullWidth
+                variant="filled"
+                sx={{ gridColumn: "span 2" }}
+              >
+                <InputLabel id="gender-label">Starting Minute</InputLabel>
+                <Select
+                  labelId="hospital-label"
+                  id="hospital"
+                  label="Hospital Branch"
+                  name="gender" // Name of the input (optional, used for form submission)
+                >
+                  {Array.from(Array(60).keys()).map((hour) => (
+                    <MenuItem key={hour} value={hour + 1}>
+                      {hour + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl
+                fullWidth
+                variant="filled"
+                sx={{ gridColumn: "span 2" }}
+              >
+                <InputLabel id="gender-label">Ending Minute</InputLabel>
+                <Select
+                  labelId="hospital-label"
+                  id="hospital"
+                  label="Hospital Branch"
+                  name="gender" // Name of the input (optional, used for form submission)
+                >
+                  {Array.from(Array(60).keys()).map((hour) => (
+                    <MenuItem key={hour} value={hour + 1}>
+                      {hour + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
             </Box>
+
             <Box display="flex" justifyContent="end" mt="20px">
               <Button type="submit" color="secondary" variant="contained">
                 Create New User
@@ -224,4 +422,4 @@ const initialValues = {
   address2: "",
 };
 
-export default Form;
+export default Farm;
