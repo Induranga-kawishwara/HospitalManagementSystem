dee98d11b50473df7554b2f91c82bef0d26e625a
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-05-07 14:13:58 +0530

    Update AddBloodDonate.jsx

--- FrontEnd/staff/src/Pages/BloodBank/AddBloodDonate.jsx
+++ FrontEnd/staff/src/Pages/BloodBank/AddBloodDonate.jsx
@@ -5,8 +5,6 @@ import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../Components/Header/Header";
 import axios from "axios";
-import firebase from "firebase/app";
-import "firebase/storage";
 
 const AddBloodDonate = () => {
   const initialValues = {
