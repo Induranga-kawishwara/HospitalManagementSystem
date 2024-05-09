944557198bb1546373714f82443d030e06c465fb
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-05-09 22:10:16 +0530

    updated

--- FrontEnd/staff/src/config/firebase.config.js
+++ FrontEnd/staff/src/config/firebase.config.js
@@ -1,11 +1,11 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBSQxU7ksIni2WRikMrFEewVokfVrT6Jvk",
-  authDomain: "hospitalmanagementsystem-235b8.firebaseapp.com",
-  projectId: "hospitalmanagementsystem-235b8",
-  storageBucket: "hospitalmanagementsystem-235b8.appspot.com",
-  messagingSenderId: "377625082587",
-  appId: "1:377625082587:web:21014a33dea4340b202afe",
-  measurementId: "G-L7SY1CR7L6",
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyBSQxU7ksIni2WRikMrFEewVokfVrT6Jvk",
+    authDomain: "hospitalmanagementsystem-235b8.firebaseapp.com",
+    projectId: "hospitalmanagementsystem-235b8",
+    storageBucket: "hospitalmanagementsystem-235b8.appspot.com",
+    messagingSenderId: "377625082587",
+    appId: "1:377625082587:web:21014a33dea4340b202afe",
+    measurementId: "G-L7SY1CR7L6",
+  },
 };
-
-export default firebaseConfig;
